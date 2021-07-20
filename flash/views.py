716533872d8ec551a77f48a5396fce920260b58b
Melby01 <melby.okozi@student.moringaschool.com>
2021-07-20 23:40:55 +0300

    INitial commit

--- flash/views.py
+++ flash/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
 
 # Create your views here.
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
