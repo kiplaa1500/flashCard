222cddc14154913787e5a087e7e8a2b25bc114c4
benowino7 <bensonowino7@gmail.com>
2021-07-21 10:11:09 +0300

    register models to admin

--- flash/admin.py
+++ flash/admin.py
@@ -1,3 +1,8 @@
+# type:ignore
 from django.contrib import admin
 
+from .models import Profile,flashCard
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(flashCard)
